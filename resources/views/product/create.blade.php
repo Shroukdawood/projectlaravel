9bb65544cf431a23bab8012193893d8e1e618bfc
shrouk <shroukdawood1990@gmail.com>
2025-04-04 19:05:19 +0200

    first commit

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Product</title>
+</head>
+<body>
+    <form action="" method="">
+        <input type="text" name="nameproduct" placeholder="Product Name">  
+        <input type="number" name="price" placeholder="Price">  
+        <input type="number" name="nameproduct" placeholder="Quantity">  
+        <button type="submit">Add</button>
+    </form>
+</body>
+</html>
